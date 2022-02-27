988b1b9279ac016ce2b6b98a111ebf4fd1e20464
Valentijn <valentijn@posteo.net>
2022-02-27 15:42:24 +0100

    [6~Rebase the code on another example code base

--- test/PartyTest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from geniusweb.party.Party import Party
-
-
-class PartyTest(unittest.TestCase):
-    def testPartyDefined(self):
-        import party
-        clas=party.party()
-        self.assertTrue(issubclass(clas, Party))
--- util/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-timeout 10 python -m geniusweb.simplerunner.NegoRunner test/resources/settings.json > /tmp/output.tst
-
-OUTPUT=$(awk '/INFO:protocol ended normally: /{print}' /tmp/output.tst | sed 's/INFO:protocol ended normally: //g')
-echo $OUTPUT
-echo $OUTPUT | jq 
